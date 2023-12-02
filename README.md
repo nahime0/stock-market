2bccae6121cc00eab8a5ab4e7c600f8806c9b412
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-12-02 20:58:39 +0100

    Added a python mock server for the Alpha vantage api

--- README.md
+++ README.md
@@ -2,3 +2,29 @@
 
 This application will periodically fetch stock prices from the [Alpha Vantage API](https://www.alphavantage.co/) and store them in a database. 
 The data can be accessed through a REST API.
+
+# Installation
+
+TBD
+
+# Running with Docker
+
+TBD
+
+# Using the provided Alpha Vantage mock server
+
+If you want to test the application, without using the real Alpha Vantage API endpoint,
+you can use the provided python mock server.
+
+Just enter the `mock` folder, activate a python virtual environment, install requirements and
+you are ready to start the server:
+
+```bash
+cd mock
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python server.py
+```
+
+The mock service will listen on the default python flask port `5000`
---
+++ mock/server.py
@@ -0,0 +1,63 @@
+import random
+from datetime import datetime, timedelta
+from flask import Flask, request
+
+app = Flask(__name__)
+# Disable sorting of JSON keys to preserve reverse date order, like the Alpha Advantage API
+app.json.sort_keys = False
+
+@app.route('/', methods=['GET'])
+def api():
+    symbol = request.args.get('symbol')
+    interval = request.args.get('interval')
+    if symbol is None:
+        return 'Please provide a symbol', 400
+
+    if interval is None:
+        return 'Please provide an interval', 400
+    elif interval == '1min':
+        minutes = 1
+    elif interval == '5min':
+        minutes = 5
+    elif interval == '15min':
+        minutes = 15
+    elif interval == '30min':
+        minutes = 30
+    elif interval == '60min':
+        minutes = 60
+    else:
+        return 'Invalid interval', 400
+
+    time_series = {}
+    now = datetime.now()
+    price_open = random.randint(100, 200)
+    for i in range(100):
+        t_datetime = now - timedelta(minutes=i * minutes)
+        price_change = random.uniform(0.99, 1.01) # +- 1%
+        price_close = price_open * price_change
+        volume = random.randint(1, 1000)
+        price_max = max([price_open, price_close])
+        price_min = min([price_open, price_close])
+        time_series[t_datetime.strftime("%Y-%m-%d %H:%M:%S")] = {
+            '1. open': "%.4f" % price_open,
+            '2. high': "%.4f" % price_max,
+            '3. low': "%.4f" % price_min,
+            '4. close': "%.4f" % price_close,
+            '5. volume': volume
+        }
+        price_open = price_close
+
+    return {
+        'Meta Data': {
+            '1. Information': "Intraday (%smin) open, high, low, close prices and volume" % minutes,
+            '2. Symbol': symbol,
+            '3. Last Refreshed': '2020-10-16 19:50:00',
+            '4. Interval': "%smin" % minutes,
+            '5. Output Size': 'Compact',
+            '6. Time Zone': 'US/Eastern'
+        },
+        "Time Series (%smin)" % minutes: time_series
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
