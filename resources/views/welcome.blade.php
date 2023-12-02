4e8dca8d42cb2ebb579a8f66bed4ae0183324bd6
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-12-02 17:42:04 +0100

    Remove fonts, swapped title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <title>Stock Market | nahime</title>
 
         <!-- Styles -->
         @vite('resources/css/app.css')
