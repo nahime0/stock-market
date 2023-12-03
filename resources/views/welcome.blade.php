0cecd1629084efd9ee1b00afd4c5cc9cd9e5e2b7
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-12-03 22:31:28 +0100

    Added AlpineJS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Styles -->
         @vite('resources/css/app.css')
+        <!-- JS -->
+        @vite('resources/js/app.js')
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
