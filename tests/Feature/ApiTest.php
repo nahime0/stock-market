0e128a9c443e11a0cb7905cfe4afdd6a3b1799ee
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-12-03 20:03:06 +0100

    Style

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -11,11 +11,11 @@ it('returns the list of symbols', function () {
     $response = $this->get('/api/symbols');
 
     $response->assertStatus(200);
-    $response->assertJson(fn(AssertableJson $json) => $json->has(10)->first(fn(AssertableJson $json) => $json
+    $response->assertJson(fn (AssertableJson $json) => $json->has(10)->first(fn (AssertableJson $json) => $json
         ->hasAll('symbol', 'name')
         ->whereAllType([
             'symbol' => 'string',
-            'name'   => 'string',
+            'name' => 'string',
         ])
     ));
 });
@@ -26,40 +26,40 @@ it('returns the ticker', function () {
     $response = $this->get("/api/ticker/{$symbol->symbol}");
     $response->assertStatus(200);
 
-    $response->assertJson(fn(AssertableJson $json) => $json
+    $response->assertJson(fn (AssertableJson $json) => $json
         ->hasAll('current', 'previous', 'change', 'change_percent')
         ->whereAllType([
-            'current'         => 'array',
-            'previous'        => 'array',
-            'change'          => 'string',
-            'change_percent'  => 'string',
+            'current' => 'array',
+            'previous' => 'array',
+            'change' => 'string',
+            'change_percent' => 'string',
         ])
         ->hasAll('current.datetime', 'current.open', 'current.high', 'current.low', 'current.close', 'current.volume')
         ->hasAll('previous.datetime', 'previous.open', 'previous.high', 'previous.low', 'previous.close', 'previous.volume')
         ->whereAllType([
             'current.datetime' => 'string',
-            'current.open'     => 'string',
-            'current.high'     => 'string',
-            'current.low'      => 'string',
-            'current.close'    => 'string',
-            'current.volume'   => 'string',
+            'current.open' => 'string',
+            'current.high' => 'string',
+            'current.low' => 'string',
+            'current.close' => 'string',
+            'current.volume' => 'string',
             'previous.datetime' => 'string',
-            'previous.open'     => 'string',
-            'previous.high'     => 'string',
-            'previous.low'      => 'string',
-            'previous.close'    => 'string',
-            'previous.volume'   => 'string',
+            'previous.open' => 'string',
+            'previous.high' => 'string',
+            'previous.low' => 'string',
+            'previous.close' => 'string',
+            'previous.volume' => 'string',
         ])
     );
 });
 
-it('returns the price history', function() {
+it('returns the price history', function () {
     $symbol = Symbol::first();
 
     $response = $this->get("/api/history/{$symbol->symbol}");
     $response->assertStatus(200);
 
-    $response->assertJson(fn(AssertableJson $json) => $json
+    $response->assertJson(fn (AssertableJson $json) => $json
         ->hasAll([
             'data',
             'data.0',
@@ -70,15 +70,15 @@ it('returns the price history', function() {
             'data.0.close',
             'data.0.volume',
             'links',
-            'meta'
+            'meta',
         ])
         ->whereAllType([
             'data.0.datetime' => 'string',
-            'data.0.open'     => 'string',
-            'data.0.high'     => 'string',
-            'data.0.low'      => 'string',
-            'data.0.close'    => 'string',
-            'data.0.volume'   => 'string',
+            'data.0.open' => 'string',
+            'data.0.high' => 'string',
+            'data.0.low' => 'string',
+            'data.0.close' => 'string',
+            'data.0.volume' => 'string',
         ])
     );
 });
