4d710638aa5171935ffe2699db8946715165961a
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-12-03 14:08:34 +0100

    Minor fix to svg image

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,8 +1,8 @@
-<svg class="{{ $class }}" viewBox="0 0 141 141" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
-    <g id="Tavola-da-disegno1" serif:id="Tavola da disegno1" transform="matrix(1.50294,0,0,1.34825,492.52,-330.233)">
+<svg class="{{ $class }}" viewBox="0 0 141 141" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve"  style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g id="Tavola-da-disegno1" transform="matrix(1.50294,0,0,1.34825,492.52,-330.233)">
         <rect x="-327.705" y="244.934" width="93.169" height="103.858" style="fill:none;"/>
     </g>
-    <g id="Livello-1" serif:id="Livello 1" transform="matrix(1,0,0,1,-569.336,-439.311)">
+    <g id="Livello-1" transform="matrix(1,0,0,1,-569.336,-439.311)">
         <g>
             <g transform="matrix(1,0,0,1,665.393,526.893)">
                 <path d="M0,-68.667L-28.876,-68.667L-28.876,-62.667L0,-62.667C15.623,-62.667 28.334,-49.956 28.334,-34.333C28.334,-18.71 15.623,-6 0,-6L-1.959,-6L-1.959,0L0,0C18.932,0 34.334,-15.401 34.334,-34.333C34.334,-53.265 18.932,-68.667 0,-68.667" style="fill:rgb(239,68,68);fill-rule:nonzero;"/>
