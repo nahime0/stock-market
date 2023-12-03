73a1cc0bf01dbb9c28d67517f2dcf4dd4624c54d
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-12-03 16:03:36 +0100

    Scheduled the FetchPricing command to run every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Console;
 
+use App\Console\Commands\FetchPricing;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(FetchPricing::class)->everyMinute();
     }
 
     /**
